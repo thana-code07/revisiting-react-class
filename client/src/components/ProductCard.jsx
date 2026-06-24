dfdde0d13085be6e8872245950326c9a309c8edd
Thanathat Lumsan <thanathatlumsan@gmail.com>
2026-06-24 11:13:36 +0700

    feat: implement ProductCard component and integrate it into ProductHighlight

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,2 +1,19 @@
 //  🛠️ Todo 1: ให้สร้าง ProductCard Component แล้วนำไป Render ในไฟล์ `ProductHighlight.jsx`
 //  แทนที่ Product Card 1, Product Card 2 และ Product Card 3
+import { ShoppingCartIcon } from "@heroicons/react/24/solid";
+
+export default function ProductCard({ src, name, price }) {
+  return (
+    <div className="bg-white p-4 shadow-md">
+      <img src={src} alt={name} className="w-full mb-2" />
+      <h3 className="text-lg font-semibold mb-1">{name}</h3>
+      <div className="flex justify-between items-center">
+        <p className="text-gray-600">{price}</p>
+        <button className="mt-2 border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white py-2 px-4 rounded flex items-center">
+          <ShoppingCartIcon className="h-4 w-4 mr-2" />
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+}
